43a5b14adb45fcbfdc1a888ebb7b4974d6bd1576
연우 <141496501+YeonWoojuice@users.noreply.github.com>
2025-01-20 20:27:01 +0900

    Title: DateTime to DateTime, Time: 42ms, Memory: 16MB, Status: Passed - CodeTree

--- 250120/DateTime to DateTime/datetime-to-datetime.py
+++ 250120/DateTime to DateTime/datetime-to-datetime.py
@@ -1,15 +1,8 @@
-from datetime import datetime
+a, b, c = map(int, input().split())
 
-# 기준 시간
-start_time = datetime(2011, 11, 11, 11, 11)
-# 입력된 시간
-end_time = datetime(2011, 11, 12, 13, 14)
+res = (a-11)*1440 + (b-11)*60 + (c-11)
 
-# 두 시간의 차이를 분 단위로 계산
-time_difference = (end_time - start_time).total_seconds() // 60
-
-# 결과 출력
-if time_difference < 0:
+if res < 0:
     print(-1)
 else:
-    print(int(time_difference))
+    print(res)
