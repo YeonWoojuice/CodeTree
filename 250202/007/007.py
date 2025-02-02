e27c7f2642b0c907ec33e0b7e958dd5000966b6d
연우 <141496501+YeonWoojuice@users.noreply.github.com>
2025-02-02 17:08:14 +0900

    Title: 007, Time: 41ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 250202/007/007.py
@@ -0,0 +1,17 @@
+
+'''secret_code, meeting_point, time = input().split()
+time = int(time)
+'''
+americano, cafelatte, cake = 5,6,7
+
+class cafe:
+    def __init__(self,americano,cafelatte,cake):
+        self.americano = americano
+        self.cafelatte=cafelatte
+        self.cake=cake
+
+    # __init__: 생성자 함수, cafe클래스의 형태를 정의 한다.
+    # 하나의 학생 객체를 만들어 내게 된다. ;.
+
+
+# Write your code here!
