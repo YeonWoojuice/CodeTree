548c8453f0cff061ecf1688b3fc1b831a03333ef
연우 <141496501+YeonWoojuice@users.noreply.github.com>
2025-02-11 15:50:04 +0900

    Title: 함수를 이용한 소수 판별, Time: 46ms, Memory: 16MB, Status: Passed - CodeTree

--- 250211/함수를 이용한 소수 판별/decimal-decisions-using-functions.py
+++ 250211/함수를 이용한 소수 판별/decimal-decisions-using-functions.py
@@ -4,12 +4,15 @@ cnt=0
 is_prime = True
 
 for i in range (a,b+1):
-    for j in range(2,i):
-        if (i%j==0 or i==1):
-            is_prime=False
+    if i==1:
+        is_prime=False
+    else:
+        for j in range(2,i):
+            if (i%j==0):
+                is_prime=False
         
-    if is_prime:
-        cnt+=i
-    is_prime = True
+        if is_prime:
+            cnt+=i
+        is_prime = True
 
 print(cnt)
