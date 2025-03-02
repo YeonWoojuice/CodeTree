5fece87c1ff639b23f61da165d72ae0e757dc296
연우 <141496501+YeonWoojuice@users.noreply.github.com>
2025-03-02 16:30:39 +0900

    Title: 함수를 이용한 부분 문자열의 위치 구하기, Time: 41ms, Memory: 16MB, Status: Passed - CodeTree

---
+++ 250302/함수를 이용한 부분 문자열의 위치 구하기/find-the-location-of-a-substring-using-a-function.py
@@ -0,0 +1,16 @@
+text = input()
+pattern = input()
+
+
+def location():
+    return text.find(pattern)
+
+print(location()) 
+
+        
+            
+
+        
+
+
+# Please write your code here.
