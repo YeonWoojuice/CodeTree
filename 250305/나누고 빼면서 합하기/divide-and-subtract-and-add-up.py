513c843a6c8f1c8e2fb5499ab01a904f4bbc493f
연우 <141496501+YeonWoojuice@users.noreply.github.com>
2025-03-05 12:34:36 +0900

    Title: 나누고 빼면서 합하기, Time: 42ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 250305/나누고 빼면서 합하기/divide-and-subtract-and-add-up.py
@@ -0,0 +1,21 @@
+n, m = map(int, input().split())
+A = list(map(int, input().split()))
+cnt=0
+
+def addmin(cnt,m,A):
+
+    while (m>0):
+        if ((m+1)%2==0):
+            m//=2
+            cnt+=A[m]
+
+        else:
+            m-=1
+            cnt+=A[m]
+
+    print(cnt)
+
+addmin(cnt,m,A)
+
+
+
