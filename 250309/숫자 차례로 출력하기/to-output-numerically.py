d43234b99277d56561fa7313e8e2d74f9244a51e
연우 <141496501+YeonWoojuice@users.noreply.github.com>
2025-03-09 15:31:06 +0900

    Title: 숫자 차례로 출력하기, Time: 44ms, Memory: 16MB, Status: Passed - CodeTree

--- 250309/숫자 차례로 출력하기/to-output-numerically.py
+++ 250309/숫자 차례로 출력하기/to-output-numerically.py
@@ -1,12 +1,22 @@
 n = int(input())
 
-def print_number(n):
+def print_number1(n):
+    if n==0:
+        return 
+    print_number1(n-1)
+    print(n,end=" ")
 
-    if (n!=0):
-        print(f"{8-n}",end=" ")
-        if (n==1):
-            print()
-        print_number(n-1)
-        print(f"{8-n}",end=" ")
+def print_number2(n):
+    if n==0:
+        return
+    print(n,end=" ")
+    print_number2(n-1)
 
-print_number(n)
+print_number1(n)
+print()
+print_number2(n)
+
+'''
+고민했던 부분: print함수 호출 사이에 print-number()을 호출 하면
+무조건 붙어서 출력되는 어떻게 두 줄로 출력 되도록 할수 있을까? 
+'''
