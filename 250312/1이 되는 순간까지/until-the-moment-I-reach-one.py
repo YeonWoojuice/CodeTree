8aa3553004aace42cd5180ff68cef336b816f01f
연우 <141496501+YeonWoojuice@users.noreply.github.com>
2025-03-12 14:44:05 +0900

    Title: 1이 되는 순간까지, Time: 49ms, Memory: 16MB, Status: Passed - CodeTree

--- 250312/1이 되는 순간까지/until-the-moment-I-reach-one.py
+++ 250312/1이 되는 순간까지/until-the-moment-I-reach-one.py
@@ -2,16 +2,16 @@ N = int(input())
 cnt=0
 
 def func(N,cnt):
+    
     if N==1:
-        print(cnt) 
+        print(cnt)
         
-    if N%2==0:
+    elif N%2==0:
         cnt+=1
         func(N//2,cnt)
         
     else:
         cnt+=1
         func(N//3,cnt)
-        
 
 func(N,cnt)
