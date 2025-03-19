d1d1cc12e2e349c5337bf68785614f2e7e949eea
연우 <141496501+YeonWoojuice@users.noreply.github.com>
2025-03-19 14:49:22 +0900

    Title: 재귀함수를 이용한 최댓값, Time: 76ms, Memory: 18MB, Status: Runtime Error - CodeTree

--- 250319/재귀함수를 이용한 최댓값/maximum-value-with-recursive-function.py
+++ 250319/재귀함수를 이용한 최댓값/maximum-value-with-recursive-function.py
@@ -2,11 +2,11 @@ n = int(input())
 arr = list(map(int, input().split()))
 
 def maximum(n,arr): #4 #3 #2
-    if (n>=2):
-        if (arr[n-1]>arr[n-2]):  # 9>7 #9>5 #9>1
-            arr[n-2]=arr[n-1] # 7=9 #5=9 #1=9
-        if n==2:
-            print(arr[0])
+    if (arr[n-1]>arr[n-2]):  # 9>7 #9>5 #9>1
+        arr[n-2]=arr[n-1] # 7=9 #5=9 #1=9
+    if n==2:
+        print(arr[0])
+
 
 def f(n,arr):
     if (n==2):
