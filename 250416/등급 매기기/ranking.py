7e3700fc7bd701bad693897bb34a1c4666c9bd99
연우 <141496501+YeonWoojuice@users.noreply.github.com>
2025-04-16 16:13:04 +0900

    Title: 등급 매기기, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

--- 250416/등급 매기기/ranking.py
+++ 250416/등급 매기기/ranking.py
@@ -1,11 +1,12 @@
 N = int(input())
-if (N<60):
+
+if N < 60:
     print("F")
-elif (N<=60 and N<70):
+elif N < 70:
     print("D")
-elif (N<=70 and N<80):
+elif N < 80:
     print("C")
-elif (N<=80 and N<90):
+elif N < 90:
     print("B")
-elif (N>=90):
+else:
     print("A")
