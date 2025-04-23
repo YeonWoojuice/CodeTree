e184b55965429615019fdd2c7f19d12e3dbcbb15
연우 <141496501+YeonWoojuice@users.noreply.github.com>
2025-04-23 14:02:15 +0900

    Title: 점수 비교, Time: 41ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 250423/점수 비교/score-comparison.py
+++ 250423/점수 비교/score-comparison.py
@@ -3,5 +3,8 @@ A_eng,B_eng = map(int, input().split(" "))
 
 if (A_math >= B_math and A_eng >= B_eng):
     print(1)
-if (A_math < B_math or A_eng < B_eng):
-    print(0)
+elif (A_math < B_math or A_eng < B_eng):
+    if (A_eng==A_math):
+        print(1)
+    else:
+        print(0)
