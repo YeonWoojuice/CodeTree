eddb67cba776b436ab78d0e38ea6d503bfc09b8a
연우 <141496501+YeonWoojuice@users.noreply.github.com>
2025-05-02 23:28:01 +0900

    Title: 더 긴 문자열, Time: 43ms, Memory: 16MB, Status: Passed - Codetree

--- 250502/더 긴 문자열/longer-string.py
+++ 250502/더 긴 문자열/longer-string.py
@@ -1,7 +1,9 @@
 A,B=map(str, input().split())
 if (len(A)<len(B)):
-    print(B)
+    print(B,end=" ")
+    print(len(B))
 elif (len(A)==len(B)):
     print("same")
 else:
-    print(A)
+    print(A,end=" ")
+    print(len(A))
