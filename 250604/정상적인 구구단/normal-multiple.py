daa43d3f20a7032cf3b178ee470877b2a5d53a44
연우 <141496501+YeonWoojuice@users.noreply.github.com>
2025-06-04 12:32:01 +0900

    Title: 정상적인 구구단, Time: 43ms, Memory: 16MB, Status: Passed - Codetree

--- 250604/정상적인 구구단/normal-multiple.py
+++ 250604/정상적인 구구단/normal-multiple.py
@@ -2,6 +2,6 @@ N=int(input())
 for i in range(1,N+1):
     for j in range(1,N+1):
         print(f"{i} * {j} = {i*j}",end="")
-        if (j!=n):
+        if (j!=N):
             print(",",end=" ")
     print()
