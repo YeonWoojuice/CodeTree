fa8781fd83db320b963b4d2b233698ba04d06c21
연우 <141496501+YeonWoojuice@users.noreply.github.com>
2025-10-20 17:51:00 +0900

    Title: 재귀함수를 이용한 3N + 1 수열, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 251020/재귀함수를 이용한 3N + 1 수열/3n-plus-1-sequence-with-recursive-function.py
@@ -0,0 +1,13 @@
+N = int(input())
+
+def f(N):
+    if N==1:
+        return 0
+    else: 
+        if N%2==0:
+            return f(N//2) +1
+        else:
+            return f(N*3+1)+1
+        
+print(f(N))
+            
