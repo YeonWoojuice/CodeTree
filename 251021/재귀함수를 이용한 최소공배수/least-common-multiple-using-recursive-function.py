3616abb5ff9a8cbc2e7a32bf1089e16a9636cec7
연우 <141496501+YeonWoojuice@users.noreply.github.com>
2025-10-21 15:05:29 +0900

    Title: 재귀함수를 이용한 최소공배수, Time: 43ms, Memory: 16MB, Status: Passed - Codetree

--- 251021/재귀함수를 이용한 최소공배수/least-common-multiple-using-recursive-function.py
+++ 251021/재귀함수를 이용한 최소공배수/least-common-multiple-using-recursive-function.py
@@ -4,7 +4,8 @@ n=int(sys.stdin.readline())
 list1=list(map(int,sys.stdin.readline().split()))
 
 def f(n, list1):
-    if n==0: return math.lcm(list1[1],list1[0])
+    if n==1: return math.lcm(list1[1],list1[0])
+    elif n==0: return list1[0]
     else:
         list1[n-1] = math.lcm(list1[n],list1[n-1])
         return f(n-1, list1)
