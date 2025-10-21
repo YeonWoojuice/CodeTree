3c2a59520b70d53dcaec3d989b0d82f1c40dd551
연우 <141496501+YeonWoojuice@users.noreply.github.com>
2025-10-21 15:02:24 +0900

    Title: 재귀함수를 이용한 최소공배수, Time: 79ms, Memory: 18MB, Status: Runtime Error - Codetree

--- 251021/재귀함수를 이용한 최소공배수/least-common-multiple-using-recursive-function.py
+++ 251021/재귀함수를 이용한 최소공배수/least-common-multiple-using-recursive-function.py
@@ -1,2 +1,11 @@
-import sys 
+import sys
+import math
 n=int(sys.stdin.readline())
+list1=list(map(int,sys.stdin.readline().split()))
+
+def f(n, list1):
+    if n==1: return math.lcm(list1[1],list1[0])
+    else:
+        list1[n-1]= math.lcm(list1[n],list1[n-1])
+        return f(n-1, list1)
+print(f(n-1,list1))
