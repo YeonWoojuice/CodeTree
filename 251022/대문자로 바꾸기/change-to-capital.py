c7a4f063b9341cc32fdb8bc57d31408d2355b1eb
연우 <141496501+YeonWoojuice@users.noreply.github.com>
2025-10-22 19:45:11 +0900

    Title: 대문자로 바꾸기, Time: 44ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 251022/대문자로 바꾸기/change-to-capital.py
+++ 251022/대문자로 바꾸기/change-to-capital.py
@@ -3,7 +3,4 @@ upper_list=[]
 for i in range(5):
     words = map(str, sys.stdin.readline().split())
 
-    for word in words:
-        upper_list.append(word.upper())
-    print(' '.join(upper_list))
-    upper_list=[]
+print(words)
