f5764cea8e8673802a55689dc23d9cc1f8fc1928
연우 <141496501+YeonWoojuice@users.noreply.github.com>
2025-10-22 21:43:34 +0900

    Title: 배열의 평균, Time: 82ms, Memory: 18MB, Status: Runtime Error - Codetree

--- 251022/배열의 평균/ave-of-array.py
+++ 251022/배열의 평균/ave-of-array.py
@@ -1,8 +1,11 @@
 import sys
-arr=[]
 for i in range(2):
-    arr=list(map(int,sys.stdin.readline().split()))
-    print(sum(arr)/4,end=" ")
-    
+    arr[i]=list(map(float,sys.stdin.readline().split()))
+    print(sum(arr[i])/4,end=" ")
+for j in range(4):
+    print(sum(list(arr[i][j] for i in range(2)))/2,end=" ")
+print(sum(arr))
+
+
 
 
