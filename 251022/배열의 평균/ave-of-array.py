0c2a0b3336234bf02a15da38ec56dbd0484578c0
연우 <141496501+YeonWoojuice@users.noreply.github.com>
2025-10-22 21:51:44 +0900

    Title: 배열의 평균, Time: 45ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 251022/배열의 평균/ave-of-array.py
+++ 251022/배열의 평균/ave-of-array.py
@@ -1,7 +1,13 @@
 import sys
+arr=[list(map(float,sys.stdin.readline().split()))for i in range(2)]
 for i in range(2):
-    arr[i]=list(map(float,sys.stdin.readline().split()))
     print(sum(arr[i])/4,end=" ")
+print()
 for j in range(4):
     print(sum(list(arr[i][j] for i in range(2)))/2,end=" ")
-print(sum(arr))
+print()
+print(sum(arr[0]+arr[1])/8)
+
+
+
+
