55ce8f3f701b2e4048ebedc6b0f50644b7669b08
연우 <141496501+YeonWoojuice@users.noreply.github.com>
2025-11-03 15:40:02 +0900

    Title: 배열의 평균, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

--- 251103/배열의 평균/ave-of-array.py
+++ 251103/배열의 평균/ave-of-array.py
@@ -4,6 +4,8 @@ arr=[list(map(int,input().strip().split()))for _ in range(2)]
 def aver(arr):
     return sum(arr)/len(arr)
 add=0
-for row in arr: print(f'{aver(row):.1f}',end=" ") print()
-for col in zip(*arr): print(f'{aver(col):.1f}',end=" ") print()
+for row in arr: print(f'{aver(row):.1f}',end=" ") 
+print()
+for col in zip(*arr): print(f'{aver(col):.1f}',end=" ") 
+print()
 print(f'{aver([x for row in arr for x in row]):.1f}')
