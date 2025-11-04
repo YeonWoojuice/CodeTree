e59e42992d3eba1f72d8e1b86f29e33c0bd72535
연우 <141496501+YeonWoojuice@users.noreply.github.com>
2025-11-04 21:30:09 +0900

    Title: 대각선으로 정수 채우기, Time: 46ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 251104/대각선으로 정수 채우기/diagonal-numbering.py
+++ 251104/대각선으로 정수 채우기/diagonal-numbering.py
@@ -13,15 +13,15 @@ for i in range(n):
         curr_row+=1
         cnt+=1
 
-curr_row = 1
-for i in range(1,m):
+
+for i in range(0,m):
     curr_col = n-1
+    curr_row = i+1
     while curr_col >= 0 and curr_row < m:
-        arr[curr_row][curr_col]=cnt
+        arr[curr_row][curr_col] = cnt
         curr_col-=1
         curr_row+=1
         cnt+=1
-    curr_row -= 1
 
 for row in arr:
     print(*row)
