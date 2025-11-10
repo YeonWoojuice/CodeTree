dbde6aba69d9390f4033bd72e102c941f8277abd
연우 <141496501+YeonWoojuice@users.noreply.github.com>
2025-11-10 18:17:08 +0900

    Title: 배열로 사각형 만들기, Time: 75ms, Memory: 18MB, Status: Runtime Error - Codetree

---
+++ 251110/배열로 사각형 만들기/print-array-in-rectangle.py
@@ -0,0 +1,10 @@
+arr=list([1 for _ in range(5)]for _ in range(5))
+for i in range(5):
+    for j in range(5):
+        if i==0 or j==0:
+            
+        
+
+for row in arr:
+    print(" ".join(map(str, row)))
+        
